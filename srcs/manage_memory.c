74fd5edebe2c362a94cccfad27741a07ce02328e
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-24 15:38:57 +0100

    added function to allocate array of t_philo and t_philo for each philospher + enhanced clean_up function to take care of newly created memory

--- srcs/manage_memory.c
+++ srcs/manage_memory.c
@@ -6,25 +6,55 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 15:49:47 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/23 16:23:35 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/24 15:37:15 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "philo.h"
 
-// int	allocate_memory(t_param *param)
-// {
-// 	t_philo *philo;
-// 	philo = param->philo;
-// 	if(!(philo = malloc(sizeof(t_philo))))
-// 		clean_up(param);
-// 	return (0);
-// }
+/*	ALLOCATE_PHILO
+*	---------------
+*	Function allocates an array of structs t_philo *, that holds a spot for a
+*	struct for each philosopher. It then allocates a struct t_philo for each
+*	philosopher.
+*/
+
+void	allocate_philo(t_param *param)
+{
+	int i;
+	
+	param->philo = malloc(sizeof(t_philo *) * param->nb_of_philos);
+	if (!param->philo)
+		clean_up(param);
+	i = 0;
+	while(i < param->nb_of_philos)
+	{
+		if (!(param->philo[i] = malloc(sizeof(t_philo))))
+			clean_up(param);
+		i++;
+	}
+}
+
+/*	CLEAN_UP
+*	----------
+*	De-allocate all allocated memory. Check for NULL pointers before attempting
+*	to access memory.
+*/
 
 int	clean_up(t_param *param)
 {
-	if (param->philo)
+	int i;
+
+	i = 0;
+	if (param->philo && param)
+	{
+		while (i < param->nb_of_philos && param->philo[i] != NULL)
+		{
+			free(param->philo[i]);
+			i++;
+		}
 		free (param->philo);
+	}
 	if (param)
 		free (param);
 	exit (0);
