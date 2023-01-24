5aefc8f1fa550a4a5983e9a38a0ad138844527ce
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-24 16:18:28 +0100

    file to play and test

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:36:12 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/23 11:30:49 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/24 10:49:17 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	*function(void *args)
 	
 	// pthread_t th = pthread_self();
 	// printf("%ul\n", th);
-	// pthread_mutex_lock(&lock);
+	pthread_mutex_lock(&lock);
 	//count++;
 	//printf("count is: %d\n", count);
 	sem_wait(&semaphore);
@@ -33,7 +33,7 @@ void	*function(void *args)
 	printf("Philo #%d calling\n", *(int*)args);
 	// printf("Philo #%d finishing\n", count);
 	
-	// pthread_mutex_unlock(&lock);
+	pthread_mutex_unlock(&lock);
 	sem_post(&semaphore);
 	free (args);
 	return (0);
@@ -51,8 +51,8 @@ int main ()
 	int *a;
 
 
-	// if (pthread_mutex_init(&lock, NULL))
-	// 	return (1);
+	if (pthread_mutex_init(&lock, NULL))
+		return (1);
 	
 	// exit (0);
 	while (i < nb_of_philos)
