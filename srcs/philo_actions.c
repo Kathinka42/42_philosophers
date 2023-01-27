7258d3cd610dff7014739633c0672f5da69b435d
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-27 14:26:31 +0100

    protected all prints with mutexes, worked on check exit function

--- srcs/philo_actions.c
+++ srcs/philo_actions.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo_states.c                                     :+:      :+:    :+:   */
+/*   philo_actions.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 09:49:16 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/24 15:56:02 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/27 14:15:55 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,12 @@ int	philo_sleep(t_philo *philo)
 
 int	philo_think(t_philo *philo)
 {
-	philo->param->timestamp = calculate_timestamp_milliseconds(philo->param);
-	printf ("%lld %d is thinking\n", philo->param->timestamp, philo->philo_nb);
-	smart_sleep(philo->param, philo->param->time_to_die);
+	// if 
+	
+	// philo->param->timestamp = calculate_timestamp_milliseconds(philo->param);
+	pthread_mutex_lock(&philo->param->time);
+	philo_print(philo, "is sleeping");
+	// smart_sleep(philo->param, philo->param->time_to_die);
 	// if ()
 	return (0);
 }
@@ -32,22 +35,29 @@ int	philo_think(t_philo *philo)
 int philo_eat(t_philo *philo)
 {
 	pthread_mutex_lock(&(philo->param->forks[philo->left_fork]));
-	printf ("%lld %d has picked up fork #%d \n", philo->param->timestamp, philo->philo_nb, philo->left_fork);
+	philo_print(philo, "has taken a fork");
+	// pthread_mutex_lock(&philo->param->write);
+	// printf ("%lld %d has picked up fork #%d \n", philo->param->timestamp, (philo->philo_nb + 1), philo->left_fork + 1);
+	// pthread_mutex_unlock(&philo->param->write);
 	pthread_mutex_lock(&(philo->param->forks[philo->right_fork]));
-	printf ("%lld %d has picked up fork #%d\n", philo->param->timestamp, philo->philo_nb, philo->right_fork);
+	philo_print(philo, "has taken a fork");
+	// pthread_mutex_lock(&philo->param->write);
+	// printf ("%lld %d has picked up fork #%d\n", philo->param->timestamp, philo->philo_nb + 1, philo->right_fork + 1);
+	// pthread_mutex_unlock(&philo->param->write);
 	philo_print(philo, "is eating");
 	philo->last_meal = philo->param->timestamp;
 	philo->nb_of_meals++;
 	smart_sleep(philo->param, philo->param->time_to_eat);
-	printf ("%lld %d has finished eating\n", philo->param->timestamp, philo->philo_nb);
+	// pthread_mutex_lock(&philo->param->write);
+	// printf ("%lld %d has finished eating\n", philo->param->timestamp, philo->philo_nb + 1);
+	// pthread_mutex_unlock(&philo->param->write);
 	pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
 	pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
 
-	// if (philo->left_fork == )
 	return (0);
 }
 
-int	check_starvation(t_param *param)
+int	check_exit(t_param *param)
 {
 	int i;
 
@@ -56,13 +66,27 @@ int	check_starvation(t_param *param)
 	{
 		while (i < param->nb_of_philos)
 		{
-			if ((calculate_timestamp_milliseconds(param) - param->philo[i]->last_meal) >  param->time_to_die)
+			if ((calculate_timestamp_milliseconds(param) - param->philo[i]->last_meal) > param->time_to_die)
 			{
 				param->philo_died = true;
-				break;
+				philo_print(param->philo[i], "died");
+				clean_up(param);
 			}
+			// if (param->philo[i]->nb_of_meals == param->philo[i]->nb_philo_must_eat)
+			// {
+			// 	param->nb_meals_reached = true;
+			// 	clean_up(param);
+			// }
 			i++;
 		}
 		i = 0;
+		// while (i < param->nb_of_philos)
+		// {
+			
+		// }
+		// i++
 	}
+	// i = 0;
+	return (0);
+	
 }
