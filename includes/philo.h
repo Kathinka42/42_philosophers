9fbbab905e9fce62416dcd7ed264f5c466c6dd5a
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-27 14:21:35 +0100

    modified structs, not finalized

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 10:03:49 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/24 15:48:49 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/27 14:14:24 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ typedef struct s_philo
 	int				left_fork;
 	int				right_fork;
 	int				nb_of_meals;
+	int				nb_philo_must_eat;
 	long long		last_meal;
 	struct s_param	*param;
 } t_philo;
@@ -38,22 +39,24 @@ typedef struct s_param
 	int				time_to_sleep;
 	int				nb_philo_must_eat;
 	bool			philo_died;
+	bool			nb_meals_reached;
 	long long		timestamp;
 	long long		start_time;
+	pthread_mutex_t time;
 	pthread_mutex_t write;
 	pthread_mutex_t	*forks;
-	pthread_mutex_t	last_meal_check;
 	t_philo			**philo;
 } t_param;
 
-// manage_memory and initializze variables
+// manage_memory and initialize variables
 // void	allocate_memory(t_param *param);
+int			ft_atoi(const char *str);
 void		allocate_philo(t_param *param);
-int			clean_up(t_param *param);
 void		init_mutexes(t_param *param);
-void		destroy_mutexes(t_param *param);
 void		init_variables(t_param *param, int argc, char **argv);
 void		init_philo(t_param *param);
+int			clean_up(t_param *param);
+void		destroy_mutexes(t_param *param);
 
 // manage time
 long long	calculate_timestamp_milliseconds(t_param *param);
@@ -66,5 +69,5 @@ int			philo_think(t_philo *philo);
 int			philo_eat(t_philo *philo);
 
 void		philo_print(t_philo *philo, char *str);
-int			check_starvation(t_param *param);
+int			check_exit(t_param *param);
 #endif
