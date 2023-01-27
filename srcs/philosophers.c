a533adcefac8ed3a343134bd1f7c755272acce0a
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-01-27 14:27:19 +0100

    added new endingg condition to handler

--- srcs/philosophers.c
+++ srcs/philosophers.c
@@ -6,11 +6,20 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 11:34:12 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/24 16:07:45 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/01/27 13:59:40 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "philo.h"
+
+/*	EXECUTE_TASKS
+*	---------------
+*	Function is main handler for pthread_create. Philos with even IDs are sent
+*	to sleep, to slightly asynchronize the start times to avoid a deadlock.
+*	Then, execution functions are called in an unterminated loop, that only
+*	ends, when either one of the philosophers dies or all philosophers have
+*	eaten the given number of times.
+*/
 
 void	*execute_tasks(void *arg)
 {
@@ -19,20 +28,27 @@ void	*execute_tasks(void *arg)
 
 	philo = arg;
 	i = 0;
-	// while (philo->param->philo_died == false)
 	while (i < philo->param->nb_of_philos)
 	{
-		philo_sleep(philo);
-		philo_eat(philo);
-		// i++;
+		if (philo->philo_nb % 2)
+			smart_sleep(philo->param, 5);
+		i++;
 	}
-	calculate_timestamp_milliseconds(philo->param);
+	while (philo->param->philo_died == false || philo->param->nb_meals_reached == false)
+	{
+		philo_eat(philo);
+		philo_sleep(philo);
+		philo_think(philo);
+	}
+	// calculate_timestamp_milliseconds(philo->param);
 	return (0);
 }
 
-/* P_thread join is making pthread create wait for the termination of the thread. After
+/*	CREATE_THREADS
+*	---------------
+*	P_thread join is making pthread create wait for the termination of the thread. After
 *	return from pthread_join, we know the thread has finished its execution. You need two
-*	while loops one for create, one for join, else ther won't be any threads working in
+*	while loops one for create, one for join, else there won't be any threads working in
 *	parallel, but only consecutively.
 */
 void	create_threads(t_param *param)
@@ -44,18 +60,16 @@ void	create_threads(t_param *param)
 	while (i < param->nb_of_philos)
 	{
 		if (pthread_create(&p[i], NULL, &execute_tasks, param->philo[i]) != 0)
-			printf("Thread not created");
+			write(STDERR_FILENO, "Thread not created", 18);
 		i++;
 	}
 	i = 0;
-	check_starvation(param);
-	printf("Failed to join thread");
+	check_exit(param);
+	// printf("Failed to join thread");
 	while (i < param->nb_of_philos)
 	{
 		if(pthread_join(p[i], NULL) != 0)
-		{
-			printf("Failed to join thread");
-		}
+			write(STDERR_FILENO, "Failed to join thread", 21);
 		// printf("Thread %d has finished its execution\n", param->philo[i]->philo_nb);
 		i++;
 	}
@@ -79,6 +93,7 @@ int main (int argc, char **argv)
 	// 	clean_up(param);
 	// allocate_memory();
 	init_variables(param, argc, argv);
+	init_mutexes(param);
 	allocate_philo(param);
 	init_philo(param);
 	philosophers(param);
