fa6b6f34e49124482ab547d77c05c7f904250f9e
Kathrin Czichowsky <kczichowsky@TCMiniMac.fritz.box>
2023-01-31 12:37:35 +0100

    included return from smart_sleep, updated mutex usage

--- srcs/philo_actions.c
+++ srcs/philo_actions.c
@@ -12,22 +12,39 @@
 
 # include "philo.h"
 
+/*	PHILO_SLEEP
+*	-----------
+*	calls print function.  Sleep function returns (1), if one of the philos
+*	died. This function transports this info by returning (1) in that case.
+*/
+
 int	philo_sleep(t_philo *philo)
 {
-	if (philo->param->philo_died == false && philo->param->nb_meals_reached == false)
-	{
-		philo_print(philo, "is sleeping");
-		smart_sleep(philo->param, philo->param->time_to_sleep);
-	}
+	philo_print(philo, "is sleeping");
+	if (smart_sleep(philo->param, philo->param->time_to_sleep))
+		return (1);
 	return (0);
 }
 
+/*	PHILO_THINK
+*	-----------
+*	only calls function to print thinking log.
+*/
+
 int	philo_think(t_philo *philo)
 {
-	if (philo->param->philo_died == false && philo->param->nb_meals_reached == false)
-		philo_print(philo, "is thinking");
+	philo_print(philo, "is thinking");
 	return (0);
 }
+
+/*	PHILO_EAT
+*	-----------
+*	most complex function called by the handler.uses mutexes to protect the
+*	forks from being taken by another philo during mealtime. returns 1, if
+*	the return from sleep funciton is (1), i.e. one philo died. else it
+*	returns (0). mutex needed to avoid data races, when incrementing the
+*	number of meals taken. 
+*/
 
 int philo_eat(t_philo *philo)
 {
@@ -35,50 +52,18 @@ int philo_eat(t_philo *philo)
 	philo_print(philo, "has taken a fork");
 	pthread_mutex_lock(&(philo->param->forks[philo->right_fork]));
 	philo_print(philo, "has taken a fork");
-	pthread_mutex_lock(&philo->param->eat);
 	philo_print(philo, "is eating");
 	philo->last_meal = calculate_timestamp_milliseconds(philo->param);
-	pthread_mutex_unlock(&philo->param->eat);
-	smart_sleep(philo->param, philo->param->time_to_eat);
+	if (smart_sleep(philo->param, philo->param->time_to_eat))
+	{
+		pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
+		pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
+		return (1);
+	}
 	pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
 	pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
+	pthread_mutex_lock(&philo->param->exit);
 	philo->nb_of_meals++;
-	return (0);
-}
-
-int	check_exit(t_param *param)
-{
-	int i;
-
-	i = 0;
-	while (1)
-	{
-		while (i < param->nb_of_philos)
-		{
-			if ((calculate_timestamp_milliseconds(param) - param->philo[i]->last_meal) > param->time_to_die)
-			{
-				pthread_mutex_lock(&param->eat);
-				param->philo_died = true;
-				philo_print(param->philo[i], "died");
-				pthread_mutex_unlock(&param->eat);
-				smart_sleep(param, 800);
-				return;
-				// clean_up(param);
-			}
-			i++;
-		}
-		i = 0;
-		while (i < param->nb_of_philos && param->philo[i]->nb_of_meals >= param->nb_philo_must_eat)
-			i++;
-		if (i == param->nb_of_philos)
-		{
-			param->nb_meals_reached = true;
-			pthread_mutex_lock(&param->eat);
-			printf("MAX NB OF MEALS REACHED\n");
-			pthread_mutex_unlock(&param->eat);
-			smart_sleep(param, 200);
-			clean_up(param);
-		}
-	}
+	pthread_mutex_unlock(&philo->param->exit);
 	return (0);
 }
