7b2756df30e4071b3465d6e947b062b5da3c6834
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-02-01 17:23:34 +0100

    added new function for check_nb_meals

--- philo/srcs/check_exit.c
+++ philo/srcs/check_exit.c
@@ -1,11 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_exit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 12:00:11 by kczichow          #+#    #+#             */
+/*   Updated: 2023/02/01 16:35:13 by kczichow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# include "philo.h"
+#include "philo.h"
 
+/*	CHECK_DEATH
+*	------------
+*	function checks if too much time has passed between beginning of last meal
+*	and now and philo has died. 
+*/
 
 void	check_death(t_param *param, int i)
 {
-    pthread_mutex_lock(&param->exit);
-	if ((calculate_timestamp_milliseconds(param) - param->philo[i]->last_meal) > param->time_to_die)
+	pthread_mutex_lock(&param->exit);
+	if ((calculate_timestamp_milliseconds(param) - param->philo[i]->last_meal) \
+		> param->time_to_die)
 	{
 		pthread_mutex_unlock(&param->exit);
 		philo_print(param->philo[i], "died");
@@ -16,29 +33,55 @@ void	check_death(t_param *param, int i)
 	else
 	{
 		pthread_mutex_unlock(&param->exit);
-		smart_sleep(param, 50);
+		usleep(10);
 	}
 }
 
+/*	CHECK_NB_MEALS
+*	---------------
+*	if no parameter for nb of meals is set, function returns 0. Else it counts
+*	from the first philo onwards, if the number of meals has been reached. It
+*	returns the number of philos that have comleted their meals until the first
+*	that has not.
+*/
+
+int	check_nb_meals(t_param *param)
+{
+	int	i;
+
+	i = 0;
+	if (param->nb_philo_must_eat < 0)
+		return (0);
+	pthread_mutex_lock(&param->exit);
+	while (i < param->nb_of_philos && param->philo[i]->nb_of_meals \
+			>= param->nb_philo_must_eat)
+		i++;
+	pthread_mutex_unlock(&param->exit);
+	return (i);
+}
+
+/*	CHECK_EXIT
+*	-----------
+*	function runs in the main process, in parallel to the threads and checks
+*	for the two terminating conditions: whether a philo dies or all philos
+*	reached the number of meals required.
+*/
+
 void	check_exit(t_param *param)
 {
-	int i;
+	int	i;
 
 	while (param->nb_meals_reached == false)
 	{
 		i = 0;
 		while (i < param->nb_of_philos)
 		{
-		    check_death(param, i);
-		    i++;
-		}
-		if (param->philo_died == true)
-			break;
-		i = 0;
-		pthread_mutex_lock(&param->exit);
-		while (i < param->nb_of_philos && param->philo[i]->nb_of_meals >= param->nb_philo_must_eat)
+			check_death(param, i);
+			if (param->philo_died == true)
+				return ;
 			i++;
-		pthread_mutex_unlock(&param->exit);
+		}
+		i = check_nb_meals(param);
 		if (i == param->nb_of_philos)
 		{
 			pthread_mutex_lock(&param->exit);
