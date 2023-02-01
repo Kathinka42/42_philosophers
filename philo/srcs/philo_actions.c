254b11c38bd50936f13dacbaf6795097ec0e3c7d
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-02-01 17:27:57 +0100

    added rule if only 1 philo to philo_eat function, changed logic of locking and unlocking mutexes to 1 2 2 1 logic

--- philo/srcs/philo_actions.c
+++ philo/srcs/philo_actions.c
@@ -6,11 +6,11 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 09:49:16 by kczichow          #+#    #+#             */
-/*   Updated: 2023/01/30 16:39:15 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/02/01 15:58:40 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "philo.h"
+#include "philo.h"
 
 /*	PHILO_SLEEP
 *	-----------
@@ -20,6 +20,8 @@
 
 int	philo_sleep(t_philo *philo)
 {
+	pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
+	pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
 	philo_print(philo, "is sleeping");
 	if (smart_sleep(philo->param, philo->param->time_to_sleep))
 		return (1);
@@ -33,35 +35,42 @@ int	philo_sleep(t_philo *philo)
 
 int	philo_think(t_philo *philo)
 {
-	philo_print(philo, "is thinking");
+	if (philo_print(philo, "is thinking"))
+		return (1);
 	return (0);
 }
 
 /*	PHILO_EAT
 *	-----------
-*	most complex function called by the handler.uses mutexes to protect the
+*	most complex function called by the handler. uses mutexes to protect the
 *	forks from being taken by another philo during mealtime. returns 1, if
 *	the return from sleep funciton is (1), i.e. one philo died. else it
 *	returns (0). mutex needed to avoid data races, when incrementing the
 *	number of meals taken. 
 */
 
-int philo_eat(t_philo *philo)
+int	philo_eat(t_philo *philo)
 {
-	pthread_mutex_lock(&(philo->param->forks[philo->left_fork]));
-	philo_print(philo, "has taken a fork");
 	pthread_mutex_lock(&(philo->param->forks[philo->right_fork]));
 	philo_print(philo, "has taken a fork");
+	if (philo->param->nb_of_philos == 1)
+	{
+		pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
+		usleep(philo->param->time_to_die + 5);
+		return (1);
+	}
+	pthread_mutex_lock(&(philo->param->forks[philo->left_fork]));
+	philo_print(philo, "has taken a fork");
 	philo_print(philo, "is eating");
+	pthread_mutex_lock(&philo->param->exit);
 	philo->last_meal = calculate_timestamp_milliseconds(philo->param);
+	pthread_mutex_unlock(&philo->param->exit);
 	if (smart_sleep(philo->param, philo->param->time_to_eat))
 	{
 		pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
 		pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
 		return (1);
 	}
-	pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
-	pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
 	pthread_mutex_lock(&philo->param->exit);
 	philo->nb_of_meals++;
 	pthread_mutex_unlock(&philo->param->exit);
