abf72dff11885af14dbd9a2355c26e468cfe9c14
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-02-06 15:59:05 +0100

    added two new functions to avoid if - else condition: every other philo now starts with the left fork to void potential data races

--- philo/srcs/philo_actions.c
+++ philo/srcs/philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 09:49:16 by kczichow          #+#    #+#             */
-/*   Updated: 2023/02/01 15:58:40 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/02/06 15:35:40 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,17 @@
 
 int	philo_sleep(t_philo *philo)
 {
-	pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
-	pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
 	philo_print(philo, "is sleeping");
+	if (philo->philo_nb % 2 == 0)
+	{
+		pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
+		pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
+	}
+	else
+	{
+		pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
+		pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
+	}
 	if (smart_sleep(philo->param, philo->param->time_to_sleep))
 		return (1);
 	return (0);
@@ -37,38 +45,75 @@ int	philo_think(t_philo *philo)
 {
 	if (philo_print(philo, "is thinking"))
 		return (1);
+	if (philo->philo_nb % 2 != 0)
+		smart_sleep(philo->param, 1);
+	else
+		smart_sleep(philo->param, 6);
 	return (0);
+}
+
+/*	PHILO_EAT_ODD_NUMBERS
+*	-----------------------
+*	to avoid deadlocks, all philos with odd IDs reach for their left forks
+*	first.
+*/
+
+void	philo_eat_odd_numbers(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->param->forks[philo->left_fork]));
+	philo_print(philo, "has taken a fork");
+	pthread_mutex_lock(&(philo->param->forks[philo->right_fork]));
+	philo_print(philo, "has taken a fork");
+	philo_print(philo, "is eating");
+	return ;
+}
+
+/*	PHILO_EAT_EVEN_NUMBERS
+*	-----------------------
+*	to avoid deadlocks, all philos with even IDs reach for their right forks
+*	first.
+*/
+
+void	philo_eat_even_numbers(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->param->forks[philo->right_fork]));
+	philo_print(philo, "has taken a fork");
+	pthread_mutex_lock(&(philo->param->forks[philo->left_fork]));
+	philo_print(philo, "has taken a fork");
+	philo_print(philo, "is eating");
+	return ;
 }
 
 /*	PHILO_EAT
 *	-----------
-*	most complex function called by the handler. uses mutexes to protect the
+*	function called by the handler. uses mutexes to protect the
 *	forks from being taken by another philo during mealtime. returns 1, if
-*	the return from sleep funciton is (1), i.e. one philo died. else it
+*	the return from sleep function is (1), i.e. one philo died. else it
 *	returns (0). mutex needed to avoid data races, when incrementing the
 *	number of meals taken. 
 */
 
 int	philo_eat(t_philo *philo)
 {
-	pthread_mutex_lock(&(philo->param->forks[philo->right_fork]));
-	philo_print(philo, "has taken a fork");
-	if (philo->param->nb_of_philos == 1)
-	{
-		pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
-		usleep(philo->param->time_to_die + 5);
-		return (1);
-	}
-	pthread_mutex_lock(&(philo->param->forks[philo->left_fork]));
-	philo_print(philo, "has taken a fork");
-	philo_print(philo, "is eating");
+	if (philo->philo_nb % 2 == 0)
+		philo_eat_even_numbers(philo);
+	else
+		philo_eat_odd_numbers(philo);
 	pthread_mutex_lock(&philo->param->exit);
 	philo->last_meal = calculate_timestamp_milliseconds(philo->param);
 	pthread_mutex_unlock(&philo->param->exit);
 	if (smart_sleep(philo->param, philo->param->time_to_eat))
 	{
-		pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
-		pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
+		if (philo->philo_nb % 2 == 0)
+		{
+			pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
+			pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
+		}
+		else
+		{
+			pthread_mutex_unlock(&(philo->param->forks[philo->left_fork]));
+			pthread_mutex_unlock(&(philo->param->forks[philo->right_fork]));
+		}
 		return (1);
 	}
 	pthread_mutex_lock(&philo->param->exit);
