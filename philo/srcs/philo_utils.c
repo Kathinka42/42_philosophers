fdccd5b0408556997599a62f3b7c1e0ad76d1c8e
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-02-07 11:50:23 +0100

    added ft_strncmp from libft

--- philo/srcs/philo_utils.c
+++ philo/srcs/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 11:04:22 by kczichow          #+#    #+#             */
-/*   Updated: 2023/02/01 16:52:49 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/02/07 11:20:57 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,4 +98,29 @@ void	*ft_calloc(size_t	count, size_t	size)
 		return (NULL);
 	ft_bzero(ptr, (count * size));
 	return (ptr);
+}
+
+/*	FT_STRNCMP
+*	-------------
+*	DESCRIPTION
+*	The function lexicographically compares n characters of two strings.
+*	Lexicographically bigger or smaller depends on the order in the alphabet
+*	(a<b);
+*/
+
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while ((i < (n - 1)) && str1[i] != '\0' && str2[i] != '\0')
+	{
+		if (str1[i] == str2[i])
+			i++;
+		else
+			return ((unsigned char)str1[i] - (unsigned char) str2[i]);
+	}
+	return ((unsigned char) str1[i] - (unsigned char) str2[i]);
 }
